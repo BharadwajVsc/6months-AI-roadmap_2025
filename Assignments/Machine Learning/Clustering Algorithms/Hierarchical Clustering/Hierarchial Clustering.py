7a129fc987ccdb632e22e650263145e011195721
Bharadwaj VSC <67839007+BharadwajVsc@users.noreply.github.com>
2025-08-12 11:13:02 +0530

    Add files via upload

---
+++ Assignments/Machine Learning/Clustering Algorithms/Hierarchical Clustering/Hierarchial Clustering.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df=pd.read_csv(r'D:\fsds\materials\class materials\august\8th- K Means Clustering\Mall_Customers.csv')
+X=df.iloc[:,[3,4]].values
+
+import scipy.cluster.hierarchy as sch
+dendogram = sch.dendrogram(sch.linkage(X, method='ward'))
+
+
+plt.title('Dendogram')
+plt.xlabel('Customers')
+plt.ylabel('ed')
+plt.show()
+
+from sklearn.cluster import AgglomerativeClustering
+
+
+hc = AgglomerativeClustering(n_clusters = 5, linkage='ward')
+y_hc = hc.fit_predict(X)
+
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+#plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+df['cluster'] =y_hc
